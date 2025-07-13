5401b0b80766765f6aef7e8d52c81c7d2d041651
bu_13a13ym_sitm <jin0722.nzw@gmail.com>
2025-07-13 20:18:46 +0900

    Initialize productList.csv, RegisterProduct.java

---
+++ src/scraping/RegisterProduct.java
@@ -0,0 +1,13 @@
+import sql.*;
+import sql.product.*;
+import sql.exception.*;
+
+public class RegisterProduct {
+    public static void main(String[] args) {
+        try {
+            SQL sql = new SQL();
+        } catch (DatabaseException e) {
+
+        }
+    }
+}
