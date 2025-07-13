3891d4511b237cbc63adfb0f323194b4996460a0
bu_13a13ym_sitm <jin0722.nzw@gmail.com>
2025-07-13 21:24:18 +0900

    edit RegisterProduct.java

--- src/scraping/RegisterProduct.java
+++ src/scraping/RegisterProduct.java
@@ -1,12 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.List;
+
 import sql.*;
 import sql.product.*;
 import sql.exception.*;
 
 public class RegisterProduct {
     public static void main(String[] args) {
+        String path = "src/scraping/productList.csv";
         try {
             SQL sql = new SQL();
+            List<String> lines = Files.readAllLines(Paths.get(path));
+            for (int i = 1; i < lines.size(); i++) {
+                String line = lines.get(i);
+                String[] fields = line.split(",");
+                int productID = 0;
+                String productName = fields[0].trim();
+                String description = fields[1].trim();
+                int totalStock = Integer.parseInt(fields[2].trim());
+                int currentStock = totalStock;
+                int numReservation = 0;
+                int rentalFee = Integer.parseInt(fields[3].trim());
+                String imageURL = fields[4].trim();
+                int rentalPeriod = Integer.parseInt(fields[5].trim());
+                LocalDate earliestRentalStart = null;
+
+                Product product = new Product(productID, productName, description, totalStock, currentStock, numReservation, rentalFee, imageURL, rentalPeriod, earliestRentalStart);
+                sql.addNewProduct(product);
+            }
         } catch (DatabaseException e) {
+
+        } catch (IOException e) {
 
         }
     }
